4ee655798c938bf324c2ec0da61fe94b0ea61d07
Fiegoldes <117885434+Fiegoldes@users.noreply.github.com>
2022-12-01 13:10:03 +0700

    Add files via upload

---
+++ Soal_2.py
@@ -0,0 +1,9 @@
+def funPalindrome () :
+    a = (input("Masukkan kata :"))
+    if a == 'katak':
+        print ("yes")
+        print("jika di balik", a)
+    else :
+        print ("nooooooo")
+        print("jika di balik", a)
+funPalindrome()
